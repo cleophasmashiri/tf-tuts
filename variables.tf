2ea080b4d90934eb2ca7153c0b20bad9fba2d602
Cleophas <cleophasmashiri@gmail.com>
2024-08-29 10:24:03 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "ami" {
    type        = string
    description = "Ubuntu AMI ID in N. Virginia Region"
-   default     = "ami-065deacbcaac64cf2"
+   default     = "ami-090abff6ae1141d7d"
 }
 
 variable "instance_type" {
